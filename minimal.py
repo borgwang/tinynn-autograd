4e1eb8e90ba0c5d03f9c210bbb642a7e25ef259a
borgwang <badbobobo@gmail.com>
2022-06-20 04:17:45 +0800

    mininal works

--- minimal.py
+++ minimal.py
@@ -1,41 +1,22 @@
-import os
-import sys
 import numpy as np
 
 from core.tensor import Tensor
-os.environ["PYOPENCL_COMPILER_OUTPUT"] = "1"
 
-np.random.seed(0)
+x = Tensor(np.random.normal(0, 1, (100, 128))).gpu()
+y = Tensor(np.random.normal(0, 1, (100, 2))).gpu()
+w = Tensor(np.random.normal(0, 1, (128, 2)), requires_grad=True).gpu()
 
-bs = 32
-odim = 8
-hdim = 2
-x = Tensor(np.random.normal(0, 1, (bs, hdim)), requires_grad=True, dtype=np.float32).gpu()
-y = Tensor(np.random.normal(0, 1, (bs, odim)), requires_grad=True, dtype=np.float32).gpu()
-
-params = {
-    "w": Tensor(np.random.normal(0, 1.0, (hdim, odim)), requires_grad=True).gpu(),
-}
-
-learing_rate = 3e-4
-loss_list = []
-for e in range(200):
-    # set gradient to zero
-    for param in params.values():
-        param.zero_grad()
-
+for epoch in range(500):
+    # zero gradients
+    w.zero_grad()
     # forward
-    pred = x @ params["w"]
+    pred = x @ w
     err = pred - y
     loss = (err * err).sum()
-
-    # backward automatically
+    # backward
     loss.backward()
+    # updata parameters
+    w -= 0.001 * w.grad
 
-    # updata parameters (gradient descent)
-    for param in params.values():
-        param -= learing_rate * param.grad
-
-    loss_list.append(loss.values)
-    if e % 10 == 0:
-        print("epoch-%i \tloss: %.4f" % (e, float(loss.values.get())))
+    if epoch % 10 == 0:
+        print(f"epoch-{epoch}  loss: {loss.values.get():.3f}")
